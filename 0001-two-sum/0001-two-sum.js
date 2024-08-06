11114abc5b75cb3907eb07017fd07dd56cb6e845
rauf-dayma <daymarauf@gmail.com>
2024-08-06 11:45:10 +0530

    Time: 127 ms (17.55%), Space: 50.1 MB (57.53%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let temp = [];
+    for (let i=0; i<nums.length; i++){
+        for (j=i+1; j<nums.length; j++){
+            if(nums[i]+nums[j] == target){
+                temp.push(i, j)
+                
+            }
+           
+        }
+       
+    }
+    return temp;
+};
